06a35c16815f93f0ad90608cc668aad6505d37e0
Bertrand Gorge <bertrand.gorge@neayi.com>
2021-04-19 15:19:21 +0200

    Moved the modals to the skin

--- resources/NeayiInteractions.js
+++ resources/NeayiInteractions.js
@@ -80,118 +80,9 @@ var neayiinteractions_controller = ( function () {
 			$( "#p-contentnavigation" ).clone( true ).appendTo( "#neayi-interaction-desktop-menu" );
 
 			// Mobile bloc is added at the top of the page
-			$(`<div class="interaction-bloc-mobile d-lg-none d-block">
-					<div class="interaction-top">
-						<div class="container px-0 interaction-buttons">
-							<div class="row mx-n1">
-								<div class="col-auto px-1">
-									<button class="btn btn-dark-green text-white neayi-interaction-applause" type="button">
-										<img src="${this.imagepath}clap.svg" width="28">
-									</button>
-									<br><span class="neayi-interaction-applause-label">1,2 k</span>
-								</div>
-								<div class="col px-1"><button class="w-100 btn btn-dark-green text-white neayi-interaction-suivre" type="button">Suivre</button><br><span class="neayi-interaction-suivre-label">350 intéressés</span></div>
-								<div class="col px-1"><button class="w-100 btn btn-dark-green text-white neayi-interaction-doneit" type="button">Je le fais</button><br><span class="neayi-interaction-doneit-label">25 exploitations</span></div>
-
-							</div>
-						</div>
-					</div>
-					<div class="container px-0 interaction-links">
-						<div class="row mx-n1">
-							<div class="col px-1"><a class="w-100 btn btn-dark-green button comments-link" href="#cs-comments"><span class="material-icons mr-1 align-middle" aria-hidden="true">arrow_downward</span>
-									<span class="questions-text">Poser une question</span></a></div>
-							<div class="col-auto pr-1 pl-0 dropdown" id="neayi-interaction-mobile-menu">
-								<button class="menu btn btn-dark-green" type="button" data-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false">
-									<span class="material-icons" aria-hidden="true">more_vert</span>
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>`).prependTo( '.contentHeader' );
-
-			// Modale - Connectez-vous
-			$(`<div class="modal fade" id="requiresLoginModal" tabindex="-1" aria-labelledby="requiresLoginModal" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header border-bottom-0">
-						<img width="200" src="/skins/skin-neayi/favicon/logo-triple-performance.svg" alt="Wiki Triple Performance">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-						</div>
-						<div class="modal-body">
-							<p>Connectez-vous ou créez un compte afin de rejoindre la communauté qui s'intéresse à ce sujet en particulier. Recevez
-							des notifications lors de nouveaux commentaires sur cette page, partagez votre expérience !</p>
-
-							<p>Cette opération ne prend qu'une minute !</p>
-						</div>
-						<div class="modal-footer border-top-0">
-							<a class="btn btn-primary" href="/index.php?title=Special:Login&returnto=${relevantPageName}">Créez un compte ou connectez-vous <span style="vertical-align: middle;" class="material-icons" aria-hidden="true">arrow_forward</span></a>
-						</div>
-					</div>
-				</div>
-			</div>`).appendTo( 'body' );
+			//$(``).prependTo( '.contentHeader' );
 			
-			// Modale - Connectez-vous (svp)
-			$(`<div class="modal fade" id="inviteLoginModal" tabindex="-1" aria-labelledby="requiresLoginModal" aria-hidden="true">
-				<div class="modal-dialog  modal-lg">
-					<div class="modal-content">
-						<div class="modal-header border-bottom-0">
-						<img width="200" src="/skins/skin-neayi/favicon/logo-triple-performance.svg" alt="Wiki Triple Performance">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-						</div>
-						<div class="modal-body">
-							<p>Merci pour vos encouragements, ils sont appréciés par l'ensemble des membres de la communauté !</p>
-
-							<p>Connectez-vous ou créez un compte afin de nous rejoindre, pour pouvoir suivre les pages, commenter ou partager votre expérience !
-								(Cette opération ne prend qu'une minute !)</p>
-						</div>
-						<div class="modal-footer border-top-0">
-							<a class="text-primary not-yet-link" data-dismiss="modal" id="tellUsMoreModalDismiss" href="#">Non merci pas encore</a>
-							<a class="btn btn-primary" href="/index.php?title=Special:Login&returnto=${relevantPageName}">Créez un compte ou connectez-vous <span style="vertical-align: middle;" class="material-icons" aria-hidden="true">arrow_forward</span></a>
-						</div>
-					</div>
-				</div>
-			</div>`).appendTo( 'body' );
-
-			// Modale - Merci
-			$(`<div class="modal fade" id="tellUsMoreModal" tabindex="-1" data-backdrop="static" aria-labelledby="tellUsMoreModal" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<form id="tellUsMoreForm">
-							<div class="modal-header border-bottom-0">
-							<h1 class="border-bottom-0">Merci !</h1>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-							</div>
-							<div class="modal-body">
-								<p>Dites en plus à la commauté pour enrichir vos échanges avec les autres agriculteurs</p>
-								<div class="row">
-									<div class="col form-group">
-										<label for="sinceInputId">Depuis quand ? <a href="#" role="button" class="badge badge-pill badge-light popover-neayi-help" data-toggle="popover" data-trigger="focus" title="Depuis quand ?" data-content="Renseignez l'année où vous avez démarré cette technique, afin d'aider les autres à comprendre votre degré d'expérience sur le sujet !">?</a></label>
-										<select class="form-control" id="sinceInputId" name="since"></select>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col form-check form-group">
-										<input class="" id="followwheck" name="follow" type="checkbox" checked value="follow">
-										<label class="form-check-label" for="followwheck">
-											Suivre la page ?
-										</label>
-									</div>
-								</div>
-							</div>
-							<div class="modal-footer border-top-0">
-								<a class="text-primary not-yet-link" data-dismiss="modal" id="tellUsMoreModalDismiss" href="#">Non merci pas encore</a>
-								<button class="btn btn-primary" id="tellUsMoreModalSubmit">Enregistrer</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>`).appendTo( 'body' );
+			$( 'a.login-links' ).attr('href', '/index.php?title=Special:Login&returnto=' + relevantPageName);
 			
 			$(function () {
 				$('.popover-neayi-help').popover()
@@ -233,14 +124,16 @@ var neayiinteractions_controller = ( function () {
 			var sessionId = mw.config.get( 'NeayiInteractions' ).wgUserSessionId;
 			var pageId = mw.config.get( 'wgArticleId' );
 			var apiToken = mw.config.get( 'NeayiInteractions' ).wgUserApiToken;
+			var headers = {};
+
+			if (apiToken != '')
+				headers.Authorization = 'Bearer ' + apiToken;
 
 			$.ajax({
 				url: "https://insights.dev.tripleperformance.fr/api/user/page/"+pageId+"?wiki_session_id=" + sessionId,
 				dataType: 'json',
 				method: "GET",				
-				headers: {
-					'Authorization': 'Bearer ' + apiToken
-				}
+				headers: headers
 			  }).done(function(data) {
 				mw.config.set('mwInteractions', data);
 
@@ -278,12 +171,14 @@ var neayiinteractions_controller = ( function () {
 			var self = this;
 
 			var insightsURL = mw.config.get( 'NeayiInteractions' ).wgInsightsRootURL;
-			var apiToken = mw.config.get( 'NeayiInteractions' ).wgUserApiToken;
 
 			var sessionId = mw.user.sessionId();
 			var pageId = mw.config.get( 'wgArticleId' );
 
-			console.log("ajaxInsights", actions, done_value);
+			var headers = {};
+			var apiToken = mw.config.get( 'NeayiInteractions' ).wgUserApiToken;
+			if (apiToken != '')
+				headers.Authorization = 'Bearer ' + apiToken;
 
 			$.ajax({
 				url: insightsURL + "api/page/"+pageId+"?wiki_session_id=" + sessionId,
@@ -293,9 +188,7 @@ var neayiinteractions_controller = ( function () {
 					interactions: actions,
 					done_value: done_value
 				},
-				headers: {
-					'Authorization': 'Bearer ' + apiToken
-				}
+				headers: headers
 			}).done(function(data) {
 				mw.config.set('mwInteractions', data);
 
