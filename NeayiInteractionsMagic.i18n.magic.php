bad71e83d415aab196f053a264393e05473b5de7
Bertrand Gorge <bertrand.gorge@neayi.com>
2023-11-21 11:56:29 +0100

    Fixed the magic words not respected

---
+++ NeayiInteractionsMagic.i18n.magic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Internationalisation file for magic words.
+ *
+ * @file
+ */
+
+$magicWords = [];
+
+/** English (English) */
+$magicWords['en'] = [
+	'no-neayi-interactions' => [ 0, 'no-neayi-interactions' ]
+];
+
+
